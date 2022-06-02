6a2d4fac34b57c86819c9aa472b6ad786a4fe4a9
Zokam-LS <lucassy07@outlook.com>
2022-06-02 00:48:14 -0500

    Correccion del static app.js e inicio del maquetado /register

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
-app.use(express.static('public'));
+app.use(express.static('pages'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'pages/home/home.html'))
