318ef1ef93138b10b67ab4725891c6e4f22fa02c
Zokam-LS <lucassy07@outlook.com>
2022-08-09 16:04:01 -0500

    Products details working by id

--- src/controllers/productsDetail.js
+++ src/controllers/productsDetail.js
@@ -2,18 +2,17 @@
 const path = require('path');
 const fs = require('fs');
 
+let productsArchive = fs.readFileSync(path.join(__dirname,'../database/products.json'));
+let products = JSON.parse(productsArchive);
+
 /*****  Controller Methods  *****/
 module.exports = {
     viewAll: (req, res) => {
-        let productsArchive = fs.readFileSync(path.join(__dirname,'../database/products.json'));
-        let products = JSON.parse(productsArchive);
         res.render('allProducts', {products});
     },
     viewDetail: (req, res) => {
-        let productsArchive = fs.readFileSync(path.join(__dirname,'../database/products.json'));
-        let products = JSON.parse(productsArchive);
         let theProductIndex = products.findIndex(prod => prod.id == req.params.id);
         let theProduct = products[theProductIndex];
-        res.render('productsDetails', {theProduct});
+        res.render('productsDetails', {product: theProduct});
     }
 }
